803288290a3f3ca0b887e506ec9a3acc85d2a4f8
znpuyn <znpuyn380@gmail.com>
2020-05-27 15:30:37 -0400

    changelog: -changed what the bot will dm a user when prompted to by the $dm command -added commands to add a tag to a user based on the location they specify from the dm message (has to be in the server channels as settings via dm is not programmed yet) -added an automated message that will dm a member on joining a server (message is the same as $dm)

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import discord
 from discord.utils import get
 from discord.ext import commands
 
-token = "NzE1MDczOTc2NDgwNjk0Mjc1.Xs36ww.gIKH3Lwspm2Hl4tWExKYBu-LQ8c"
+token = open("key.txt", "r")
 client = discord.Client()
 
 @client.event
@@ -24,17 +24,58 @@ async def on_message(message):
     server = client.get_guild (715077503948947500) #needs to be somewhere where the bot has already initialized if not it returns none. 
     if message.author == client.user:
         return
+
+
     if message.content.startswith('$hello'):
         await message.channel.send(f"Hello there {message.author.name}")
+
+
     if message.content.startswith('$numofusers'):
         await message.channel.send (f"```{server.member_count}```")
+
+
     if message.content.startswith('$dm'):
-        await message.author.send ("hi there 👀")
+        await message.author.send (""""Hello I am the official AI Academy bot, Reginald!
+    You might notice that the AI Academy discord is empty, but that's not the case. 
+    First, let me ask you, what locationg are you from?
+    1. Nautilus Middle 
+    2. Kinloch Park Middle
+    3. NMB Library""")
+
+
     if message.content.startswith('$roles'):
-        await message.channel.send(f'your roles are {message.author.roles([1:])
+        await message.channel.send(f'your roles are {message.author.roles[1:]}')
+
+
+    if message.content.startswith("$location 1"):
+        if message.guild.roles[4] not in message.author.roles[1:]:
+            await message.author.add_roles(message.guild.roles[1], reason="user responded with 1", atomic = True)
+            await message.channel.send("I have added the appropriate tags to your user")
+        if message.guild.roles[4] in message.author.roles[1:]:
+            await message.channel.send("You already have that tag")
+
+    if message.content.startswith("$location 2"):
+        if message.guild.roles[2] not in message.author.roles[1:]:
+            await message.author.add_roles(message.guild.roles[1], reason="user responded with 1", atomic = True)
+            await message.channel.send("I have added the appropriate tags to your user")
+        if message.guild.roles[2] in message.author.roles[1:]:
+            await message.channel.send("You already have that tag")
+
+    if message.content.startswith("$location 3"):
+        if message.guild.roles[3] not in message.author.roles[1:]:
+            await message.author.add_roles(message.guild.roles[1], reason="user responded with 1", atomic = True)
+            await message.channel.send("I have added the appropriate tags to your user")
+        if message.guild.roles[3] in message.author.roles[1:]:
+            await message.channel.send("You already have that tag")
+
 
 @client.event
 async def on_member_join(member):
-    await member.send("hello there")
+    await member.send("""Hello I am the official AI Academy bot, Reginald!
+    You might notice that the AI Academy discord is empty, but that's not the case. 
+    First, let me ask you, what locationg are you from?
+    1. Nautilus Middle 
+    2. Kinloch Park Middle
+    3. NMB Library""")
 
-client.run(token)
+client.run(token.read())
